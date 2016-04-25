b5c5f007416677ec16710e1f9d158f27da6b330f
“Brian <brianlens@gmail.com>
2016-04-25 15:30:54 +0200

    models, schema, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.create([
+    {title: "Seed them DB y'all", completed:true},
+    {title: "Migrate if aftah"},
+    {title: "Create tasks controller"},
+    {title: "Create routing"},
+    {title: "Print out some JSON"}
+  ])
+
+puts "Seeded #{Task.count} Tasks!"
